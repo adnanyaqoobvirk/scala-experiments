2dc4fc4e301a102c4eaa4fe0239745e471b1fdd0
Adnan Yaqoob <adnan@yaqoob.org>
2017-07-31 20:09:11 +0800

    adds array problem statements

--- src/main/scala/org/yaqoob/scala/hackerearth/MatrixTranspose.scala
+++ src/main/scala/org/yaqoob/scala/hackerearth/MatrixTranspose.scala
@@ -2,6 +2,15 @@ package org.yaqoob.scala.hackerearth
 
 import scala.io.StdIn
 
+//Given a 2D array A, your task is to convert all rows to columns and columns to rows.
+//
+//Input:
+//First line contains 2 space separated integers, N - total rows, M - total columns.
+//Each of the next N lines will contain M space separated integers.
+//
+//Output:
+//Print M lines each containing N space separated integers.
+
 object MatrixTranspose extends App {
   val Array(rows, cols) = StdIn.readLine.split(" ").map(_.toInt)
   val matrix = (0 until rows).map(_ => StdIn.readLine).map(_.split(" ").map(_.toInt)).toArray
--- src/main/scala/org/yaqoob/scala/hackerearth/PrintArrayInReverse.scala
+++ src/main/scala/org/yaqoob/scala/hackerearth/PrintArrayInReverse.scala
@@ -2,6 +2,15 @@ package org.yaqoob.scala.hackerearth
 
 import scala.io.StdIn
 
+//Given the size and the elements of array A, print all the elements in reverse order.
+//
+//Input:
+//First line of input contains, N - size of the array.
+//Following N lines, each contains one integer, i{th} element of the array i.e. A[i].
+//
+//Output:
+//Print all the elements of the array in reverse order, each element in a new line.
+
 object PrintArrayInReverse extends App {
   val arraySize = StdIn.readInt
   (0 until arraySize).map(_ => StdIn.readInt).reverse.foreach(println)
