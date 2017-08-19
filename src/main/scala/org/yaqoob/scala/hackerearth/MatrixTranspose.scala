07148560e4b64952d840978b18c0cede7c6268a9
Adnan Yaqoob <adnan@yaqoob.org>
2017-08-19 23:12:39 +0800

    improves matrix transpose solution

--- src/main/scala/org/yaqoob/scala/hackerearth/MatrixTranspose.scala
+++ src/main/scala/org/yaqoob/scala/hackerearth/MatrixTranspose.scala
@@ -14,5 +14,5 @@ import scala.io.StdIn
 object MatrixTranspose extends App {
   val Array(rows, cols) = StdIn.readLine.split(" ").map(_.toInt)
   val matrix = (0 until rows).map(_ => StdIn.readLine).map(_.split(" ").map(_.toInt)).toArray
-  (0 until cols).foreach(i => println(matrix.map(row => s"${row(i)} ").reduce(_ + _).trim))
+  (0 until cols).foreach(i => println(matrix.map(row => row(i)).mkString(" ")))
 }
